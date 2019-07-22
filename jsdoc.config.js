ee8a0914fac5fdde8ccdc27a90db6f88ecd722a2
Bryan Housel <bryan@mapbox.com>
2019-07-22 14:01:59 -0400

    Use docdash theme for docs, and run them through prettier too

--- jsdoc.config.js
+++ jsdoc.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   opts: {
     destination: './docs/',
-    recurse: true
+    recurse: true,
+    template: 'node_modules/docdash'
   },
   source: {
     excludePattern: '(node_modules|docs|tests)'
@@ -9,5 +10,27 @@ module.exports = {
   tags: {
     allowUnknownTags: false,
     dictionaries: ['jsdoc']
+  },
+
+  docdash: {
+    sort: true,
+    openGraph: {
+      title: 'ideditor',
+      type: 'Map editing made easy',
+      url: 'https://github.com/ideditor/ideditor'
+    },
+    meta: {
+      title: 'iD Editor Documentation'
+    },
+    search: true,
+    private: false,
+    menu: {
+      GitHub: {
+        href: 'https://github.com/ideditor/ideditor',
+        target: '_blank',
+        class: 'menu-item',
+        id: 'GitHub'
+      }
+    }
   }
 };
