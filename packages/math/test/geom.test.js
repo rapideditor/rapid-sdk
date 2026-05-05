1710c08e24118ae1dc9aef26b6ef98498ce47bf2
Bryan Housel <bhousel@gmail.com>
2026-05-05 15:19:41 -0400

    Improve surrounding rectangle heuristics and tests
    closes #267
    closes https://github.com/facebook/Rapid/issues/1045

--- packages/math/test/geom.test.js
+++ packages/math/test/geom.test.js
@@ -9,6 +9,39 @@ assert.closeTo = function(a, b, epsilon = 1e-9) {
   }
 };
 
+function rectangleSideLengths(rectangle) {
+  const poly = rectangle.poly;
+  const sideA = Math.hypot(poly[1][0] - poly[0][0], poly[1][1] - poly[0][1]);
+  const sideB = Math.hypot(poly[2][0] - poly[1][0], poly[2][1] - poly[1][1]);
+  return sideA > sideB ? [sideA, sideB] : [sideB, sideA];
+}
+
+function rectangleArea(rectangle) {
+  const [longSide, shortSide] = rectangleSideLengths(rectangle);
+  return longSide * shortSide;
+}
+
+function assertRectangleContainsPoints(rectangle, points) {
+  const origin = rectangle.poly[0];
+  const edgeA = [rectangle.poly[1][0] - origin[0], rectangle.poly[1][1] - origin[1]];
+  const edgeB = [rectangle.poly[3][0] - origin[0], rectangle.poly[3][1] - origin[1]];
+  const edgeALengthSquare = edgeA[0] * edgeA[0] + edgeA[1] * edgeA[1];
+  const edgeBLengthSquare = edgeB[0] * edgeB[0] + edgeB[1] * edgeB[1];
+
+  for (const point of points) {
+    const pointVector = [point[0] - origin[0], point[1] - origin[1]];
+    const alongA = (pointVector[0] * edgeA[0] + pointVector[1] * edgeA[1]) / edgeALengthSquare;
+    const alongB = (pointVector[0] * edgeB[0] + pointVector[1] * edgeB[1]) / edgeBLengthSquare;
+    assert.ok(alongA >= -1e-9 && alongA <= 1 + 1e-9);
+    assert.ok(alongB >= -1e-9 && alongB <= 1 + 1e-9);
+  }
+}
+
+function offAxisLShapedBuildingPoints() {
+  const footprint = [[0, 0], [8, 0], [8, 2], [3, 2], [3, 6], [0, 6], [0, 0]];
+  return math.geomRotatePoints(footprint, Math.PI / 6, [0, 0]);
+}
+
 describe('math/geom', () => {
   describe('geomEdgeEqual', () => {
     it('returns false for inequal edges', () => {
@@ -274,15 +307,63 @@ describe('math/geom', () => {
       assert.equal(math.geomGetSmallestSurroundingRectangle([]), null);
     });
 
-    it('calculates a smallest surrounding rectangle', () => {
-      //  +-- p1 ------ p3
-      //  |              |
-      //  p0 ------ p2 --+
-      const points = [[0, -1], [5, 1], [10, -1], [15, 1]];
+    it('normalizes nearly equivalent axis angles', () => {
+      const points = [[0, 0], [10, -1e-13], [10, 2], [0, 2], [0, 0]];
       const ssr = math.geomGetSmallestSurroundingRectangle(points);
       assert.ok(ssr instanceof Object);
-      assert.deepEqual(ssr.poly, [[0, -1], [15, -1], [15, 1], [0, 1], [0, -1]]);
       assert.equal(ssr.angle, 0);
+    });
+
+    it('calculates a smallest surrounding rectangle for an off-axis L-shaped building', () => {
+      //  p5 --- p4
+      //  |      |
+      //  |      p3 ------ p2
+      //  |                |
+      //  p0 ------------- p1
+      const points = offAxisLShapedBuildingPoints();
+      const ssr = math.geomGetSmallestSurroundingRectangle(points);
+      assert.ok(ssr instanceof Object);
+      assertRectangleContainsPoints(ssr, points);
+
+      const [longSide, shortSide] = rectangleSideLengths(ssr);
+      assert.closeTo(longSide, 8);
+      assert.closeTo(shortSide, 6);
+      assert.closeTo(ssr.angle, Math.PI / 6);
+    });
+  });
+
+  describe('geomGetLongestSurroundingRectangle', () => {
+    it('returns null for empty points array', () => {
+      assert.equal(math.geomGetLongestSurroundingRectangle([]), null);
+    });
+
+    it('calculates a longest surrounding rectangle for an off-axis L-shaped building', () => {
+      //  p5 --- p4
+      //  |      |
+      //  |      p3 ------ p2
+      //  |                |
+      //  p0 ------------- p1
+      const points = offAxisLShapedBuildingPoints();
+      const lsr = math.geomGetLongestSurroundingRectangle(points);
+      assert.ok(lsr instanceof Object);
+      assertRectangleContainsPoints(lsr, points);
+
+      const [longSide, shortSide] = rectangleSideLengths(lsr);
+      assert.closeTo(longSide, 9.995120760870789);
+      assert.closeTo(shortSide, 6.559297999321455);
+      assert.closeTo(lsr.angle, 1.4196541601696426);
+    });
+
+    it('can choose a longer envelope than the smallest surrounding rectangle', () => {
+      const points = offAxisLShapedBuildingPoints();
+      const smallest = math.geomGetSmallestSurroundingRectangle(points);
+      const longest = math.geomGetLongestSurroundingRectangle(points);
+
+      assert.ok(smallest instanceof Object);
+      assert.ok(longest instanceof Object);
+      assert.ok(Math.abs(smallest.angle - longest.angle) > 1e-9);
+      assert.ok(rectangleSideLengths(longest)[0] > rectangleSideLengths(smallest)[0]);
+      assert.ok(rectangleArea(longest) > rectangleArea(smallest));
     });
   });
 
